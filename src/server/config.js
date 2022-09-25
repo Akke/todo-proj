8307fdf43c98b39883f107fa4bae221fcc7ff606
Axwell <atomwar.box@gmail.com>
2022-09-26 01:54:27 +0200

    Update config.js

--- src/server/config.js
+++ src/server/config.js
@@ -2,17 +2,17 @@ const config = {
     url: "http://localhost:3000",
     steam: {
         returnUrl: "auth/steam/return",
-        apiKey: "7FD4AE2FAC4E9EF8CC7002E9BA4463D0",
+        apiKey: "",
 
     },
     google: {
-        clientId: "498962547213-rvla2qgev9k0m3fvc852ep14pa2k8f1l.apps.googleusercontent.com",
-        clientSecret: "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",
+        clientId: "",
+        clientSecret: "",
         callbackUrl: "auth/google/callback"
     },
     discord: {
-        clientId: "1023670078857814138",
-        clientSecret: "ENwL4V1-fSmFLgTSgstD_KLXJy8ucRrZ",
+        clientId: "",
+        clientSecret: "",
         callbackUrl: "auth/discord/callback"
     }
 }
